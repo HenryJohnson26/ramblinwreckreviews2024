4b5557d27a4b247b89dbd724c04d64791b5146da
unknown <jgilliland@mines.edu>
2024-09-01 10:43:19 -0600

    added post to insert faculty into users table

--- src/components/pages/admin/Admin_Home.jsx
+++ src/components/pages/admin/Admin_Home.jsx
@@ -3,20 +3,19 @@ import Error401 from '../error/Error401';
 import '../../../css/Admin_Home.css';
 import AWS_Authenticator from '../../AWS_Authenticator';
 import AuthStatusEnum from '../../../types/AuthStatusEnum';
+import Admin_AddFaculty from "./Admin_AddFaculty";
+import axios from "axios";
 
 
 function Admin_Home() {
   return (
-    <div>
-      <>
-      <link rel="stylesheet" href="style.css" />
-      <body>
-        <div className='admin-home-content-container'>
-          {<MenuBar />}
-          <div className="admin-home-welcomeText"><h1>Welcome to the admin home page</h1></div>
-        </div>
-      </body>
-      </>
+    <div className="admin-home-body-frame">
+      <div className="admin-home-menu-bar-container">
+        <MenuBar />
+      </div>
+      <div className="admin-home-content-container">
+        <Admin_AddFaculty />
+      </div>
     </div>
   );
 }
